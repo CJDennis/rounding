4ab9a2b199d695cb3a733d1538c42658cc6d2388
Daniel Klein <danielklein@airpost.net>
2019-10-09 22:12:37 +1100

    Pass the tests

--- src/CJDennis/Rounding/Rounding.php
+++ src/CJDennis/Rounding/Rounding.php
@@ -2,7 +2,10 @@
 namespace CJDennis\Rounding;
 
 class Rounding {
-  public static function round($number, $precision = 0) {
-    return round($number, $precision);
+  public static function round($number, int $new_precision = 0, int $old_precision = null) {
+    $old_precision = $old_precision ?? $new_precision;
+    $rounded_to_old_precision_int = round($number * pow(10, $old_precision));
+    $floored_to_new_precision_int = floor($rounded_to_old_precision_int * pow(10, $new_precision - $old_precision));
+    return $floored_to_new_precision_int / pow(10, $new_precision);
   }
 }

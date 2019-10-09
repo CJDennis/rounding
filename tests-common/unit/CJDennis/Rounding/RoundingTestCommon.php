43aeb4bff6b9ce5c9724a89104ea132f58720a7f
Daniel Klein <danielklein@airpost.net>
2019-10-09 22:03:16 +1100

    Add tests to check rounding a number to three decimal places

--- tests-common/unit/CJDennis/Rounding/RoundingTestCommon.php
+++ tests-common/unit/CJDennis/Rounding/RoundingTestCommon.php
@@ -16,4 +16,12 @@ trait RoundingTestCommon {
   public function testShouldRoundTheFractionalPartOfANumberExactlyAHalfUpToTheNextInteger() {
     $this->assertSame(2.0, Rounding::round(1.5));
   }
+
+  public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesBelowAHalfDownToTheNextInteger() {
+    $this->assertSame(1.499, Rounding::round(1.499499, 3));
+  }
+
+  public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesExactlyAHalfUpToTheNextInteger() {
+    $this->assertSame(1.5, Rounding::round(1.4995, 3));
+  }
 }

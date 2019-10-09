4fb9795b9608d0a5efccf2d295ab63b2a5ac0db6
Daniel Klein <danielklein@airpost.net>
2019-10-09 22:35:59 +1100

    Change assertSame which uses an epsilon value to assertTrue and exact equality

--- tests-common/unit/CJDennis/Rounding/RoundingTestCommon.php
+++ tests-common/unit/CJDennis/Rounding/RoundingTestCommon.php
@@ -10,26 +10,26 @@ trait RoundingTestCommon {
 
   // tests
   public function testShouldRoundTheFractionalPartOfANumberBelowAHalfDownToTheNextInteger() {
-    $this->assertSame(1.0, Rounding::round(1.499999));
+    $this->assertTrue(1.0 === Rounding::round(1.499999));
   }
 
   public function testShouldRoundTheFractionalPartOfANumberExactlyAHalfUpToTheNextInteger() {
-    $this->assertSame(2.0, Rounding::round(1.5));
+    $this->assertTrue(2.0 === Rounding::round(1.5));
   }
 
   public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesBelowAHalfDownToTheNextInteger() {
-    $this->assertSame(1.499, Rounding::round(1.499499, 3));
+    $this->assertTrue(1.499 === Rounding::round(1.499499, 3));
   }
 
   public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesExactlyAHalfUpToTheNextInteger() {
-    $this->assertSame(1.5, Rounding::round(1.4995, 3));
+    $this->assertTrue(1.5 === Rounding::round(1.4995, 3));
   }
 
   public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesBelowAHalfDownToTheNextIntegerConsideringFourDecimalPlaces() {
-    $this->assertSame(1.499, Rounding::round(1.499499, 3, 4));
+    $this->assertTrue(1.499 === Rounding::round(1.499499, 3, 4));
   }
 
   public function testShouldRoundTheFractionalPartOfANumberAfterThreeDecimalPlacesExactlyAHalfUpToTheNextIntegerConsideringFourDecimalPlaces() {
-    $this->assertSame(1.499, Rounding::round(1.4995, 3, 4));
+    $this->assertTrue(1.499 === Rounding::round(1.4995, 3, 4));
   }
 }

517bef7c151f2c6c1c8639ea4405203202a89350
Daniel Klein <danielklein@airpost.net>
2019-10-09 22:03:45 +1100

    Pass the tests

--- src/CJDennis/Rounding/Rounding.php
+++ src/CJDennis/Rounding/Rounding.php
@@ -2,7 +2,7 @@
 namespace CJDennis\Rounding;
 
 class Rounding {
-  public static function round($number) {
-    return round($number);
+  public static function round($number, $precision = 0) {
+    return round($number, $precision);
   }
 }

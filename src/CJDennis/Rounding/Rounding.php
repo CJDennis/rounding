2cc8d5b990491b76699a465872d6a905a15dcee1
Daniel Klein <danielklein@airpost.net>
2019-10-15 23:11:43 +1100

    Pass the tests

--- src/CJDennis/Rounding/Rounding.php
+++ src/CJDennis/Rounding/Rounding.php
@@ -11,24 +11,44 @@ class Rounding {
 
   public static function round_fraction_up($number, int $new_precision = 0, int $old_precision = null) {
     if ($old_precision === null) {
-      $adjusted_to_precision_int = $number * pow(10, $new_precision);
-      $ceiling_to_precision_int = ceil($adjusted_to_precision_int);
+      if ($number === 0.0) {
+        $ceiling_to_precision_int = $number;
+      }
+      else {
+        $adjusted_to_precision_int = $number * pow(10, $new_precision);
+        $ceiling_to_precision_int = ceil($adjusted_to_precision_int);
+      }
     }
     else {
-      $adjusted_to_precision_int = round($number * pow(10, $old_precision));
-      $ceiling_to_precision_int = ceil($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
+      if ($number === 0.0) {
+        $ceiling_to_precision_int = $number;
+      }
+      else {
+        $adjusted_to_precision_int = round($number * pow(10, $old_precision));
+        $ceiling_to_precision_int = ceil($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
+      }
     }
     return $ceiling_to_precision_int / pow(10, $new_precision);
   }
 
   public static function round_fraction_down($number, int $new_precision = 0, int $old_precision = null) {
     if ($old_precision === null) {
-      $adjusted_to_precision_int = $number * pow(10, $new_precision);
-      $floored_to_precision_int = floor($adjusted_to_precision_int);
+      $adjusted_to_precision_int = $number * pow(10, $new_precision) + 0;
+      if ($number === 0.0) {
+        $floored_to_precision_int = $number * $adjusted_to_precision_int;
+      }
+      else {
+        $floored_to_precision_int = floor($adjusted_to_precision_int);
+      }
     }
     else {
-      $adjusted_to_precision_int = round($number * pow(10, $old_precision));
-      $floored_to_precision_int = floor($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
+      if ($number === 0.0) {
+        $floored_to_precision_int = $number;
+      }
+      else {
+        $adjusted_to_precision_int = round($number * pow(10, $old_precision));
+        $floored_to_precision_int = floor($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
+      }
     }
     return $floored_to_precision_int / pow(10, $new_precision);
   }

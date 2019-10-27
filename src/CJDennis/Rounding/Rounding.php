cd00e4342aed015ac012fde9df45f6cfaed0a0fa
Daniel Klein <danielklein@airpost.net>
2019-10-27 13:54:37 +1100

    Refactor round_fraction_up()

--- src/CJDennis/Rounding/Rounding.php
+++ src/CJDennis/Rounding/Rounding.php
@@ -10,23 +10,16 @@ class Rounding {
   }
 
   public static function round_fraction_up($number, int $new_precision = 0, int $old_precision = null) {
-    if ($old_precision === null) {
-      if ($number === 0.0) {
-        $ceiling_to_precision_int = $number;
-      }
-      else {
+    $ceiling_to_precision_int = $number;
+    if ($number !== 0.0) {  // both 0.0 and -0.0
+      if ($old_precision === null) {
+        $old_precision = $new_precision;
         $adjusted_to_precision_int = $number * pow(10, $new_precision);
-        $ceiling_to_precision_int = ceil($adjusted_to_precision_int);
-      }
-    }
-    else {
-      if ($number === 0.0) {
-        $ceiling_to_precision_int = $number;
       }
       else {
         $adjusted_to_precision_int = round($number * pow(10, $old_precision));
-        $ceiling_to_precision_int = ceil($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
       }
+      $ceiling_to_precision_int = ceil($adjusted_to_precision_int * pow(10, $new_precision - $old_precision));
     }
     return $ceiling_to_precision_int / pow(10, $new_precision);
   }
